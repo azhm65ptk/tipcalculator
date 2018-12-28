eca4278eda79e339794e1b51dafea7c3ef65fb50
Aung Myint <Austin@aung-zin-hein-myints-macbook-pro.local>
2018-12-28 13:59:37 -0500

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Aung Myint on 12/28/18.
+//  Copyright © 2018 Aung Myint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
